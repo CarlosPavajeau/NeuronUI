0eaae7f44794501806a107886d450a240231a8cb
Carlos Pavajeau <cantte098@gmail.com>
2021-03-25 12:21:01 -0500

    feat: Add ErrorChart UserControl

---
+++ NeuronUI/ErrorChart.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace NeuronUI
+{
+    /// <summary>
+    /// Interaction logic for ErrorChart.xaml
+    /// </summary>
+    public partial class ErrorChart : UserControl
+    {
+        public ErrorChart()
+        {
+            InitializeComponent();
+        }
+    }
+}
