b26c4c36e166871218d37dea372c0ba435432a30
Carlos Pavajeau <cantte098@gmail.com>
2021-03-30 22:22:43 -0500

    feat: Add Neuron simulation user control

---
+++ NeuronUI/NeuronSimulation.xaml.cs
@@ -0,0 +1,148 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using LiveCharts.Defaults;
+using Microsoft.Win32;
+using NeuronUI.Data;
+using NeuronUI.Models;
+using NeuronUI.Models.ViewModels;
+using NeuronUI.Utils;
+
+namespace NeuronUI
+{
+    /// <summary>
+    /// Interaction logic for NeuronSimulation.xaml
+    /// </summary>
+    public partial class NeuronSimulation : UserControl
+    {
+        private NeuronViewModel NeuronViewModel { get; set; }
+
+        private List<List<double>> Inputs { get; set; }
+        private List<double> ExpectedOutputs { get; set; }
+        private List<double> ObtainedOutputs { get; set; } = new();
+
+        private List<SimulationData> SimulationData { get; set; }
+
+        public NeuronSimulation()
+        {
+            InitializeComponent();
+
+            Loaded += new RoutedEventHandler(OnLoaded);
+
+            SimulationData = new();
+            DataTable.ItemsSource = SimulationData;
+        }
+
+        public NeuronSimulation(ReturnToTraining returnToTraining) : this()
+        {
+            ReturnToTraining += returnToTraining;
+        }
+
+        public event ReturnToTraining ReturnToTraining;
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            NeuronViewModel = DataContext as NeuronViewModel;
+        }
+
+        private static List<string[]> LoadCsvDataFromFile()
+        {
+            string fileName = SelectFile();
+            if (string.IsNullOrEmpty(fileName))
+            {
+                return null;
+            }
+
+            List<string[]> data = CsvDataLoader.LoadCsv(fileName);
+            return data;
+        }
+
+        private static string SelectFile()
+        {
+            OpenFileDialog openFileDialog = new()
+            {
+                DefaultExt = "*.csv",
+                Filter = "CSV Files (*.csv)|*.csv|TXT Files (*.txt)|*.txt"
+            };
+
+            bool? result = openFileDialog.ShowDialog();
+
+            return result.HasValue && result.Value ? openFileDialog.FileName : string.Empty;
+        }
+
+        private void LoadSimulationDataButton_Click(object sender, RoutedEventArgs e)
+        {
+            var data = LoadCsvDataFromFile();
+            if (data is null && data.Count == 0)
+            {
+                return;
+            }
+
+            Inputs = new();
+            ExpectedOutputs = new();
+
+            foreach (string[] item in data)
+            {
+                List<double> inputs = new();
+                for (int i = 0; i < item.Length - 1; i++)
+                {
+                    if (double.TryParse(item[i], out double inputResult))
+                    {
+                        inputs.Add(inputResult);
+                    }
+                }
+
+                if (inputs.Any())
+                {
+                    Inputs.Add(inputs);
+                }
+
+                if (double.TryParse(item.Last(), out double outputResult))
+                {
+                    ExpectedOutputs.Add(outputResult);
+                }
+            }
+
+            PatternsCountChip.Content = $"Patrones: {Inputs.Count}";
+            StartSimulationButton.IsEnabled = true;
+        }
+
+        private void StartSimulationButton_Click(object sender, RoutedEventArgs e)
+        {
+            for (int i = 0; i < Inputs.Count; i++)
+            {
+                var inputs = Inputs[i];
+                double obtainedOutput = NeuronViewModel.Neuron.Output(inputs.ToArray());
+
+                string inputsStr = string.Empty;
+                for (int j = 0; j < inputs.Count; j++)
+                {
+                    inputsStr += $"{inputs[j]}";
+                    if (j < inputs.Count - 1)
+                    {
+                        inputsStr += ", ";
+                    }
+                }
+                SimulationData simulationData = new()
+                {
+                    Inputs = inputsStr,
+                    ObtainedOutput = obtainedOutput,
+                    ExpectedOutput = ExpectedOutputs[i]
+                };
+
+                SimulationData.Add(simulationData);
+
+                NeuronViewModel.VersusSeries[0].Values.Add(new ObservableValue(ExpectedOutputs[i]));
+                NeuronViewModel.VersusSeries[1].Values.Add(new ObservableValue(obtainedOutput));
+                ObtainedOutputs.Add(obtainedOutput);
+                DataTable.Items.Refresh();
+            }
+        }
+
+        private void ReturnToTrainingButton_Click(object sender, RoutedEventArgs e)
+        {
+            ReturnToTraining?.Invoke();
+        }
+    }
+}
